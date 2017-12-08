ca66440d3b565a44d9f73d0927855086f87cfb35
Yang <yangdai713@aim.com>
2017-12-08 15:16:25 -0500

    First Upload, needs some modification for the ship speed and pics

---
+++ alien_invisions.py
@@ -0,0 +1,49 @@
+import pygame
+from pygame.sprite import Group
+from time import sleep
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+def run_game():
+    # initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+    # Make the play button.
+    play_button = Button(ai_settings, screen, "play")
+    
+    # Create an instance to store game statistics and create a scoreboard.
+    stats= GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    
+    #Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    
+    #Creat the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)             
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+        
+        sleep(0.003)
+ 
+
+
+run_game()
